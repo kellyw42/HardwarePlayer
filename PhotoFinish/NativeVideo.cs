6fec5695f613c62a940ea864b1096779387f19c7
Wayne Kelly <kellyw@qut.edu.au>
2017-12-21 11:12:41 +1000

    Added C# photo finish project

---
+++ PhotoFinish/NativeVideo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public delegate void timehandler(int start, int now);
+public delegate void eventhandler(int event_type);
+
+namespace PhotoFinish
+{
+    class NativeVideo
+    {
+        [DllImport("HardwarePlayer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Init();
+
+        [DllImport("HardwarePlayer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CreateVideoWindow(eventhandler event_handler, timehandler time_handler, String filename, int monitor);
+
+        [DllImport("HardwarePlayer.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void EventLoop();
+    }
+}
